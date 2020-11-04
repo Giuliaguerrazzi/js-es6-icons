0cecbe9b464b4df6989542431c64b2843b437eb6
Giulia <giulia.guerrazzi@live.it>
2020-11-04 20:00:02 +0100

    add color icons

--- main.js
+++ main.js
@@ -101,12 +101,22 @@ const icons = [
     },
   ];
 
+  const colors = [
+    'blue',
+    'orange',
+    'purple'
+  ]
+
   // riferimento di elemento nel dom
   const container = $('.icons');
 
   // print ICONS
-  printIcons(icons, container);
+  // printIcons(icons, container);
 
+  // print con colori
+  const coloredIcons = colorIcons(icons, colors);
+  console.log(coloredIcons);
+  printIcons(coloredIcons, container);
 }); //end doc
 
 
@@ -117,14 +127,53 @@ function printIcons(icons, container) {
 
   icons.forEach((icon) => {
     // destrutturazione di icon
-    const {family, prefix, name} = icon;
+    const {family, prefix, name, color} = icon;
 
     const html =
     `<div class="icon">
-       <i class="${family} ${prefix}${name}"></i>
+       <i class="${family} ${prefix}${name}"
+       style="color: ${color}"></i>
        <div class="title">${name}</div>
     </div>`;
 
     container.append(html);
   });
+}
+
+// colorare le icone
+function colorIcons (icons, colors) {
+  // ottenere i type
+  const types = getType(icons);
+  console.log(types);
+
+  // assegnare il colore
+  // creare una array senza toccare l'originale, iterare sulle icone
+  const coloredIcons = icons.map((icon) => {
+    // sfrutto gli indici degli elementi, con indexoff per ottenere gli indici in base al valore
+    const indexType = types.indexOf(icon.type);
+
+    // itero e faccio una copia esatta
+    return {
+      ...icon,
+      color: colors[indexType]
+    }
+  });
+
+  return coloredIcons;
+}
+
+// ottenere le icone in base al tipo
+function getType(icons) {
+  const types = [];
+
+  // iterare sulle icone
+  icons.forEach((icon) => {
+    // se non è già presente lo inserisco
+    if(! types.includes(icon.type)) {
+      types.push(icon.type);
+    }
+  });
+
+  // viene ritornata fuori ed assegnata alla variabile
+  return types;
 }
